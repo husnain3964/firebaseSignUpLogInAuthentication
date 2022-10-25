67076b8fdc8c88b177e6a8e159e5e41fedb01106
Husnain Shah <106562772+husnain3964@users.noreply.github.com>
2022-10-25 05:37:36 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import AppRouter from './Config/router';
+
+function App() {
+  return (
+   
+   <div>
+
+<AppRouter/>
+
+
+   </div>
+   
+   
+   );
+}
+
+export default App;
